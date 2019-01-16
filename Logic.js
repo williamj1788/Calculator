daa2e3086f14075ed69aae38c9c95c010b20004b
Jacquez Williams <williamj1788@student.faytechcc.edu>
2019-01-15 21:03:05 -0500

    Add files via upload

---
+++ Logic.js
@@ -0,0 +1,27 @@
+
+var buttons = document.getElementsByTagName("BUTTON");
+var titleBarText = document.querySelector(".title").querySelector('p');
+
+buttons = Array.from(buttons);
+
+var eventHandler = (element) =>{
+	let addString = () => {
+		let text = element.innerHTML;
+		let originalText = titleBarText.innerHTML;
+		let newText = originalText + text;
+		titleBarText.innerHTML = newText;
+	};
+	element.addEventListener('click',addString);
+};
+
+var ConvertToNumber = () =>{
+	let text = titleBarText.innerHTML;
+	let number = eval(text);
+	titleBarText.innerHTML = number.toString(10);
+};
+
+enterButton = buttons.filter(element => element.innerHTML == 'Enter');
+buttons = buttons.filter(element => element.innerHTML != 'Enter');
+buttons.forEach(eventHandler);
+enterButton[0].addEventListener('click',ConvertToNumber);
+console.log(eval('5+3*8'));
